952648efc22b939ce24410a4db243a892b9b6b19
NoxelNyx <tnova@azdes.gov>
2023-07-09 19:18:10 -0700

    testing new colors

--- js/groove_writer.js
+++ js/groove_writer.js
@@ -65,9 +65,9 @@ function GrooveWriter() {
 	var constant_default_tempo = 80;
 	var constant_note_stem_off_color = "transparent";
 	var constant_note_on_color_hex = "#000000"; // black
-	var constant_note_on_color_rgb = 'rgb(0, 0, 0)'; // black
+	var constant_note_on_color_rgb = rgb(206, 147, 216); // black
 	var constant_note_off_color_hex = "#FFF";
-	var constant_note_off_color_rgb = 'rgb(255, 255, 255)'; // white
+	var constant_note_off_color_rgb = 'rgb(18, 18, 18)'; // white
 	var constant_note_border_color_hex = "#999";
 	var constant_hihat_note_off_color_hex = "#CCC";
 	var constant_hihat_note_off_color_rgb = 'rgb(204, 204, 204)'; // grey
@@ -79,7 +79,7 @@ function GrooveWriter() {
 	var constant_sticking_right_off_color_rgb = "rgb(204, 204, 204)";
 	var constant_sticking_left_off_color_rgb = "rgb(204, 204, 204)";
 	var constant_snare_accent_on_color_hex = "#FFF";
-	var constant_snare_accent_on_color_rgb = "rgb(255, 255, 255)";
+	var constant_snare_accent_on_color_rgb = "rgb(18, 18, 18)";
 
 	// functions below
 
