fcc6db070f92b204b3202ea18d4ad13937bb863e
NoxelNyx <tnova@azdes.gov>
2023-07-09 19:41:13 -0700

    css

--- js/groove_writer.js
+++ js/groove_writer.js
@@ -66,20 +66,20 @@ function GrooveWriter() {
 	var constant_note_stem_off_color = "transparent";
 	var constant_note_on_color_hex = "#121212"; // black
 	var constant_note_on_color_rgb = 'rgb(18, 18, 18)'; // black
-	var constant_note_off_color_hex = "#CCC";
+	var constant_note_off_color_hex = "#FFF";
 	var constant_note_off_color_rgb = 'rgb(18, 18, 18)'; // white
 	var constant_note_border_color_hex = "#999";
     var constant_hihat_note_on_color_hex = "#CE93D8"; // black
-	var constant_hihat_note_off_color_hex = "#CCC";
+	var constant_hihat_note_off_color_hex = "#FFF";
 	var constant_hihat_note_off_color_rgb = 'rgb(204, 204, 204)'; // grey
 	var constant_note_hidden_color_rgb = "transparent";
-	var constant_sticking_right_on_color_rgb = "rgb(36, 132, 192)";
+	var constant_sticking_right_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_left_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_both_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_count_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_right_off_color_rgb = "rgb(204, 204, 204)";
 	var constant_sticking_left_off_color_rgb = "rgb(204, 204, 204)";
-	var constant_snare_accent_on_color_hex = "#ccc";
+	var constant_snare_accent_on_color_hex = "#CE93D8";
 	var constant_snare_accent_on_color_rgb = "rgb(204, 204, 204)";
 
 	// functions below
